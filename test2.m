9d8769448524bb175d1fff807ed90fad6feb64e9
Emma Edvardsson <emmed608@student.liu.se>
2014-11-17 13:22:07 +0100

    Switch statement

--- test2.m
+++ test2.m
@@ -4,79 +4,118 @@ nrOfCols = 41;
 
 %img1 = imread('D:\Skola\TNM034\Images_Training_1\Bygg_1.png');
 img1 = imread('Images_Training_1/Bygg_1.png');
-%figure
-%imshow(img1(:,1))
 
 %Normalize
 img1_n = im2bw(img1, graythresh(img1));
 
-%disk element, open close
-% se = strel('disk', 5);
-% se2 = strel('square', 5);
-% img1_o = imopen(img1_n,se);
-% img1_c = imclose(img1_o,se);
-% 
-% img1_o = imopen(img1_n,se2);
-
-
-
 imSize = size(img1_n);
 
-%bitW = round(imSize(1)/nrOfCols);
-%bitH = round(imSize(2)/nrOfCols);
-
-%figure
 imshow(img1_n)
 
-array = zeros(8, 180);
-pos = 1;
-fmark = 0;
-row = 1;
-col = 1;
+% status
+% 0: White border around QR-code
+% 1: Outer black border of fiducial mark
+% 2: Outer white border of fiducial mark
+% 3: Black square of fiducial mark
+% 4: Same as 2
+% 5: Same as 1
+% 6: extra case.. 
 
+status = 0;
+blackCounter = 0;
+whiteCounter = 0;
+centerCounter = 0;
+startX = 0;
+startY = 0;
 
-
-%[Gmag, Gdir] = imgradient(img1_n,'prewitt');
-
-%imshow(Gmag);
-%C=corner(Gmag, 'MinimumEigenvalue', 600)
-
-hold on
-%plot(C(:,1),C(:,2), 'r*');
-
-%title('Gradient Magnitude, Gmag (left), and Gradient Direction, Gdir (right), using Prewitt method')
-%axis off;
-
-%array(n,1) = colors
-%array(n,2) = start pos
-%array(n,3) = end pos
-%array(n,4) = nr in a row
-arraySize = imSize(1);
-array = zeros(arraySize, 4);
-previousColor = -1;
-arrayRow = 0;
 %Loop through image to enter repetitions of colors...
-for j = 1:1:imSize(2)
-    for i = 1:1:imSize(1)
-        switch n
-            case 1
-                %Counting black pixels in a row
+for j = 1:20:imSize(2)
+    for i = 1:20:imSize(1)
+        switch status
+            case 0
+                if(img1_n(i,j) == 1)
+                    blackCounter = 0;
+                    status = 1;
+                end
+            case 1 
+                if(img1_n(i,j) == 1)
+                    blackCounter = blackCounter + 1;
+                else
+                    startX = i;
+                    startY = j;
+                    whiteCounter = 0;
+                    status = 2;
+                end
+                
             case 2
-                %Counting white pixels in a row. If != nr of black pixels
-                %go back to case 1 and count new black pixels
-                %(egentligen 0.5-1.5 * nr of black pixels)
+                if(img1_n(i,j) == 0)
+                    whiteCounter = whiteCounter + 1;
+                else
+                    if(whiteCounter == blackCounter)
+                        centerCounter = 0;
+                        status = 3;
+                    else
+                        blackCounter = 0;
+                        status = 0;
+                        
+                    end
+                end
             case 3
-                %Counting black pixels in a row, if != 3*nr of black/white
-                %pixels from case 1 & 2, go back to case 1
-                %(egentligen 2.5-3.5 * nr of black/white pixels
+                if(img1_n(i,j) == 1)
+                    centerCounter = centerCounter + 1;
+                else
+                    if(centerCounter == 3*blackCounter)
+                        whiteCounter = 0;
+                        status = 4;
+                    else
+                        blackCounter = 0;
+                        status = 0;
+                    end
+                end
             case 4
-                %Counting white pixels in a row, if != nr of pixels from
-                %case 1/2
-                %(egentligen 0.5-1.5 * nr of black pixels)
+                if(img1_n(i,j) == 0)
+                    whiteCounter = whiteCounter + 1;
+                else
+                    if(whiteCounter == blackCounter)
+                        blackCounter = 0;
+                        status = 5;
+                    else
+                        blackCounter = 0;
+                        status = 0;
+                    end
+                end
             case 5
-                %Counting black pixels in a row, if != nr of pixels from
-                %case 1/2/4
-                %(egentligen 0.5-1.5 * nr of black pixels)
+                if(img1_n(i,j) == 1)
+                    blackCounter = blackCounter + 1;
+                else
+                    if(blackCounter == whiteCounter)
+                        whiteCounter = 0;
+                        status = 6;
+                    else
+                        status = 0;
+                    end
+                end
+            case 6
+%                 if(img1_n(i,j) == 0)
+%                     whiteCounter = whiteCounter + 1;
+%                 else
+%                     if(whiteCounter == blackCounter)
+%                         blackCounter = 0;
+%                         status = 7;
+%                     else
+%                         blackCounter = 0;
+%                         status = 1;
+%                     end
+%                 end
+%             case 7
+                disp('Fiducial!')
+                disp(i)
+                disp(j)
+                disp(whiteCounter);
+                disp(blackCounter);
+                status = 0;
+                figure
+                imshow(img1_n((startX:i),(startY:j)));
         end
     end
 end
