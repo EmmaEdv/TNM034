fc32b071e796c6d11edc92ee363c8037a806e8bb
Emma Edvardsson <emmed608@student.liu.se>
2014-11-17 16:06:01 +0100

    Nice lines between points

--- test2.m
+++ test2.m
@@ -21,7 +21,8 @@ imshow(img1_n)
 % 4: Same as 2
 % 5: Same as 1
 % 6: extra case.. 
-
+vertical = zeros(1000, 4);
+n = 1;
 status = 0;
 blackCounter = 0;
 whiteCounter = 0;
@@ -36,8 +37,10 @@ for j = 1:1:imSize(2)
     for i = 1:1:imSize(1)
         switch status
             case 0
-                if(img1_n(i,j) == 0)
-                    blackCounter = 0;
+                if(img1_n(i,j) == 1)
+                    whiteCounter = whiteCounter + 1;
+                else
+                    blackCounter = 1;
                     status = 1;
                     startX = i;
                     startY = j;
@@ -49,7 +52,6 @@ for j = 1:1:imSize(2)
                     whiteCounter = 1;
                     status = 2;
                 end
-                
             case 2
                 if(img1_n(i,j) == 1)
                     whiteCounter = whiteCounter + 1;
@@ -58,7 +60,8 @@ for j = 1:1:imSize(2)
                         centerCounter = 1;
                         status = 3;
                     else
-                        blackCounter = 0;
+                        blackCounter = 1;
+                        whiteCounter = 0;
                         status = 0;
                         
                     end
@@ -72,6 +75,7 @@ for j = 1:1:imSize(2)
                         status = 4;
                     else
                         blackCounter = 0;
+                        whiteCounter = 1;
                         status = 0;
                     end
                 end
@@ -84,7 +88,8 @@ for j = 1:1:imSize(2)
                         blackCounter = 1;
                         status = 5;
                     else
-                        blackCounter = 0;
+                        blackCounter = 1;
+                        whiteCounter = 0;
                         status = 0;
                     end
                 end
@@ -97,31 +102,30 @@ for j = 1:1:imSize(2)
                         whiteCounter = 1;
                         status = 6;
                     else
+                        blackCounter = 0;
+                        whiteCounter = 1;
                         status = 0;
                     end
                 end
             case 6
-%                 if(img1_n(i,j) == )
-%                     whiteCounter = whiteCounter + 1;
-%                 else
-%                     if(whiteCounter == blackCounter)
-%                         blackCounter = 0;
-%                         status = 7;
-%                     else
-%                         blackCounter = 0;
-%                         status = 1;
-%                     end
-%                 end
-%             case 7
-                %disp('Fiducial!')
-                %disp(i)
-                %disp(j)
-                %disp(whiteCounter);
-                %disp(blackCounter);
+                if(img1_n(i,j) == 1)
+                    whiteCounter = whiteCounter + 1;
+                
+                else
+                    if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
+                        status = 7;
+                    else
+                        blackCounter = 1;
+                        whiteCounter = 0;
+                        status = 0;
+                    end
+                end
+            case 7
                 status = 0;
-                %figure
-                %imshow(img1_n((startX:i),(startY:j)));
                 hold on
+                vertical(n,:) = [startX, startY, i, j];
+                line([vertical(n,2), vertical(n,4)], [vertical(n,1), vertical(n,3)], 'color', [0.0,0.5,0.0]);
+                n = n+1;
                 plot(startY, startX, '*g')
                 plot(j, i, '+r')
         end
@@ -131,14 +135,15 @@ end
 
 
 
-
+horizontal = zeros(1000, 4);
+m=1;
 %Horizontal scanning
 for i = 1:1:imSize(1)
     for j = 1:1:imSize(2)
         switch status
             case 0
                 if(img1_n(i,j) == 0)
-                    blackCounter = 0;
+                    blackCounter = 1;
                     status = 1;
                     startX = i;
                     startY = j;
@@ -202,29 +207,26 @@ for i = 1:1:imSize(1)
                     end
                 end
             case 6
-%                 if(img1_n(i,j) == )
-%                     whiteCounter = whiteCounter + 1;
-%                 else
-%                     if(whiteCounter == blackCounter)
-%                         blackCounter = 0;
-%                         status = 7;
-%                     else
-%                         blackCounter = 0;
-%                         status = 1;
-%                     end
-%                 end
-%             case 7
-                %disp('Fiducial!')
-                %disp(i)
-                %disp(j)
-                %disp(whiteCounter);
-                %disp(blackCounter);
+                if(img1_n(i,j) == 1)
+                    whiteCounter = whiteCounter + 1;
+                else
+                    if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
+                        blackCounter = 0;
+                        status = 7;
+                    else
+                        blackCounter = 0;
+                        status = 1;
+                    end
+                end
+            case 7
                 status = 0;
-                %figure
-                %imshow(img1_n((startX:i),(startY:j)));
                 hold on
+                horizontal(m,:) = [startX, startY, i, j];
+                line([horizontal(m,2), horizontal(m,4)], [horizontal(m,1), horizontal(m,3)]);
+                m = m+1;
                 plot(startY, startX, '*b')
                 plot(j, i, '+y')
+                
         end
     end
 end
