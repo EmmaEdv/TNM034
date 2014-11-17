2734efe94b552073d3f778d67e10f7b773e07d53
Emma Edvardsson <emmed608@student.liu.se>
2014-11-17 16:54:57 +0100

    y = kx+m

--- test2.m
+++ test2.m
@@ -4,6 +4,7 @@ nrOfCols = 41;
 
 %img1 = imread('D:\Skola\TNM034\Images_Training_1\Bygg_1.png');
 img1 = imread('Images_Training_1/Bygg_1.png');
+%img1 = imread('Images_Training_3/Hus_2a.png');
 %img1 = imread('Images_Training_5/Husannons_full.png');
 
 %Normalize
@@ -21,11 +22,18 @@ imshow(img1_n)
 % 4: Same as 2
 % 5: Same as 1
 % 6: extra case.. 
-vertical = zeros(1000, 4);
+
+%y=kx + m function
+slope = @(line) (line(2) - line(1))/(line(4) - line(3));
+
+vertical = zeros(1000, 6);
 n = 1;
 status = 0;
 blackCounter = 0;
 whiteCounter = 0;
+blackCounter2 = 0;
+whiteCounter2 = 0;
+
 centerCounter = 0;
 startX = 0;
 startY = 0;
@@ -71,7 +79,7 @@ for j = 1:1:imSize(2)
                     centerCounter = centerCounter + 1;
                 else
                     if(centerCounter >= 2.5*blackCounter && centerCounter <= 3.5*blackCounter)
-                        whiteCounter = 1;
+                        whiteCounter2 = 1;
                         status = 4;
                     else
                         blackCounter = 0;
@@ -81,50 +89,53 @@ for j = 1:1:imSize(2)
                 end
             case 4
                 if(img1_n(i,j) == 1)
-                    whiteCounter = whiteCounter + 1;
+                    whiteCounter2 = whiteCounter2 + 1;
                 else
-                    if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
+                    if(whiteCounter2 >= 0.5*whiteCounter && whiteCounter2 <= 1.5*whiteCounter)
                     %if(whiteCounter == blackCounter)
-                        blackCounter = 1;
+                        blackCounter2 = 1;
                         status = 5;
                     else
                         blackCounter = 1;
-                        whiteCounter = 0;
+                        whiteCounter2 = 0;
                         status = 0;
                     end
                 end
             case 5
                 if(img1_n(i,j) == 0)
-                    blackCounter = blackCounter + 1;
+                    blackCounter2 = blackCounter2 + 1;
                 else
-                    if(blackCounter >= 0.5*whiteCounter && blackCounter <= 1.5*whiteCounter)
+                    if(blackCounter2 >= 0.5*blackCounter && blackCounter2 <= 1.5*blackCounter)
                     %if(blackCounter == whiteCounter)
-                        whiteCounter = 1;
+                        whiteCounter2 = 1;
                         status = 6;
                     else
-                        blackCounter = 0;
-                        whiteCounter = 1;
+                        blackCounter2 = 0;
+                        whiteCounter2 = 1;
                         status = 0;
                     end
                 end
             case 6
-                if(img1_n(i,j) == 1)
-                    whiteCounter = whiteCounter + 1;
-                
-                else
-                    if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
-                        status = 7;
-                    else
-                        blackCounter = 1;
-                        whiteCounter = 0;
-                        status = 0;
-                    end
-                end
-            case 7
+%                 if(img1_n(i,j) == 1)
+%                     whiteCounter2 = whiteCounter2 + 1;
+%                 
+%                 else
+%                     if(whiteCounter2 >= 0.5*whiteCounter && whiteCounter2 <= 1.5*whiteCounter)
+%                         status = 7;
+%                     else
+%                         blackCounter2 = 1;
+%                         whiteCounter2 = 0;
+%                         status = 0;
+%                     end
+%                 end
+%             case 7
                 status = 0;
                 hold on
-                vertical(n,:) = [startX, startY, i, j];
+                vertical(n,1:4) = [startX, startY, i, j];
                 line([vertical(n,2), vertical(n,4)], [vertical(n,1), vertical(n,3)], 'color', [0.0,0.5,0.0]);
+                k = slope(vertical(n,1:4));
+                vertical(n,5) = k;
+                
                 n = n+1;
                 plot(startY, startX, '*g')
                 plot(j, i, '+r')
@@ -134,8 +145,13 @@ end
 
 
 
+blackCounter = 0;
+whiteCounter = 0;
+blackCounter2 = 0;
+whiteCounter2 = 0;
 
-horizontal = zeros(1000, 4);
+
+horizontal = zeros(1000, 6);
 m=1;
 %Horizontal scanning
 for i = 1:1:imSize(1)
@@ -207,22 +223,24 @@ for i = 1:1:imSize(1)
                     end
                 end
             case 6
-                if(img1_n(i,j) == 1)
-                    whiteCounter = whiteCounter + 1;
-                else
-                    if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
-                        blackCounter = 0;
-                        status = 7;
-                    else
-                        blackCounter = 0;
-                        status = 1;
-                    end
-                end
-            case 7
+%                 if(img1_n(i,j) == 1)
+%                     whiteCounter = whiteCounter + 1;
+%                 else
+%                     if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
+%                         blackCounter = 0;
+%                         status = 7;
+%                     else
+%                         blackCounter = 0;
+%                         status = 1;
+%                     end
+%                 end
+%             case 7
                 status = 0;
                 hold on
-                horizontal(m,:) = [startX, startY, i, j];
+                horizontal(m,1:4) = [startX, startY, i, j];
                 line([horizontal(m,2), horizontal(m,4)], [horizontal(m,1), horizontal(m,3)]);
+                k = slope(horizontal(m,1:4));
+                horizontal(m,5) = k;
                 m = m+1;
                 plot(startY, startX, '*b')
                 plot(j, i, '+y')
@@ -230,3 +248,15 @@ for i = 1:1:imSize(1)
         end
     end
 end
+
+% %FIX DIS!!!!!
+% intercept = @(line,m) line(1,2) - m*line(1,1);
+% b1 = intercept(line1,m1)
+% b2 = intercept(line2,m2)
+% xintersect = (b2-b1)/(m1-m2)
+% yintersect = m1*xintersect + b1
+% 
+% 
+% plot(xintersect,yintersect,'m*','markersize',8)
+% legend({'line1','line2','intersection'},'Location','West')
+% hold off
