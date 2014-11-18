7e678e909b2781ee70feb65a574697ae106e0475
Emma Edvardsson <emmed608@student.liu.se>
2014-11-18 12:00:55 +0100

    Något bättre linedetector för vertical

--- test2.m
+++ test2.m
@@ -28,13 +28,12 @@ slope = @(line) (line(2) - line(1))/(line(4) - line(3));
 
 intercept = @(line,m) line(1) - m*line(3);
 
-vertical = zeros(1000, 6);
+%1000 ?R KANSKE INTE J?TTEBRA V?RDE EGENTLIGEN...
+vertical = zeros(1000, 7);
 n = 1;
 status = 0;
 blackCounter = 0;
 whiteCounter = 0;
-blackCounter2 = 0;
-whiteCounter2 = 0;
 
 centerCounter = 0;
 startX = 0;
@@ -47,9 +46,9 @@ for j = 1:1:imSize(2)
     for i = 1:1:imSize(1)
         switch status
             case 0
-                if(img1_n(i,j) == 1)
-                    whiteCounter = whiteCounter + 1;
-                else
+                if(img1_n(i,j) == 0)
+%                     whiteCounter = whiteCounter + 1;
+%                 else
                     blackCounter = 1;
                     status = 1;
                     startY = i;
@@ -66,84 +65,64 @@ for j = 1:1:imSize(2)
                 if(img1_n(i,j) == 1)
                     whiteCounter = whiteCounter + 1;
                 else
-                    if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
+                    if(whiteCounter >= 0.75*blackCounter && whiteCounter <= 1.75*blackCounter)
                         centerCounter = 1;
                         status = 3;
                     else
-                        blackCounter = 1;
-                        whiteCounter = 0;
                         status = 0;
-                        
                     end
                 end
             case 3
                 if(img1_n(i,j) == 0)
                     centerCounter = centerCounter + 1;
                 else
-                    if(centerCounter >= 2.5*blackCounter && centerCounter <= 3.5*blackCounter)
-                        whiteCounter2 = 1;
+                    if(centerCounter >= 2.75*blackCounter && centerCounter <= 3.75*blackCounter)
+                        whiteCounter = 1;
                         status = 4;
                     else
-                        blackCounter = 0;
-                        whiteCounter = 1;
                         status = 0;
                     end
                 end
             case 4
                 if(img1_n(i,j) == 1)
-                    whiteCounter2 = whiteCounter2 + 1;
+                    whiteCounter = whiteCounter + 1;
                 else
-                    if(whiteCounter2 >= 0.5*whiteCounter && whiteCounter2 <= 1.5*whiteCounter)
-                    %if(whiteCounter == blackCounter)
-                        blackCounter2 = 1;
+                    if(whiteCounter >= 0.75*blackCounter && whiteCounter <= 1.75*blackCounter)
+                        blackCounter = 1;
                         status = 5;
                     else
-                        blackCounter = 1;
-                        whiteCounter2 = 0;
                         status = 0;
                     end
                 end
             case 5
                 if(img1_n(i,j) == 0)
-                    blackCounter2 = blackCounter2 + 1;
+                    blackCounter = blackCounter + 1;
                     stopY = i;
                     stopX = j;
                 else
-                    if(blackCounter2 >= 0.5*blackCounter && blackCounter2 <= 1.5*blackCounter)
+                    if(blackCounter >= 0.75*whiteCounter && blackCounter <= 1.75*whiteCounter)
                     %if(blackCounter == whiteCounter)
-                        whiteCounter2 = 1;
+                        whiteCounter = 1;
                         status = 6;
                     else
-                        blackCounter2 = 0;
-                        whiteCounter2 = 1;
                         status = 0;
                     end
                 end
             case 6
-%                 if(img1_n(i,j) == 1)
-%                     whiteCounter2 = whiteCounter2 + 1;
-%                 
-%                 else
-%                     if(whiteCounter2 >= 0.5*whiteCounter && whiteCounter2 <= 1.5*whiteCounter)
-%                         status = 7;
-%                     else
-%                         blackCounter2 = 1;
-%                         whiteCounter2 = 0;
-%                         status = 0;
-%                     end
-%                 end
-%             case 7
                 status = 0;
                 hold on
-                vertical(n,1:4) = [startY, startX, i, j];
+                vertical(n,1:4) = [startY, startX, stopY, stopX];
                 %Draw lines:
                 line([vertical(n,2), vertical(n,4)], [vertical(n,1), vertical(n,3)], 'color', [0.0,0.5,0.0]);
                 %Calculate k and b of equation y=kx+b
                 k = slope(vertical(n,1:4));
                 b = intercept(vertical(n, 1:4), k);
-                vertical(n,5) = k;
-                vertical(n,6) = b;
+                vertical(n,6) = k;
+                vertical(n,7) = b;
                 
+                midY = startY + (stopY-startY)/2;
+                vertical(n,5) = midY;
+                plot(startX, midY, '+w');
                 n = n+1;
                 plot(startX, startY, '+g')
                 plot(stopX, stopY, '+r')
@@ -188,7 +167,6 @@ for i = 1:1:imSize(1)
                         centerCounter = 1;
                         status = 3;
                     else
-                        blackCounter = 0;
                         status = 0;
                         
                     end
@@ -201,7 +179,6 @@ for i = 1:1:imSize(1)
                         whiteCounter = 1;
                         status = 4;
                     else
-                        blackCounter = 0;
                         status = 0;
                     end
                 end
@@ -214,7 +191,6 @@ for i = 1:1:imSize(1)
                         blackCounter = 1;
                         status = 5;
                     else
-                        blackCounter = 0;
                         status = 0;
                     end
                 end
@@ -225,7 +201,6 @@ for i = 1:1:imSize(1)
                     stopX = j;
                 else
                     if(blackCounter >= 0.5*whiteCounter && blackCounter <= 1.5*whiteCounter)
-                    %if(blackCounter == whiteCounter)
                         whiteCounter = 1;
                         status = 6;
                     else
@@ -233,33 +208,24 @@ for i = 1:1:imSize(1)
                     end
                 end
             case 6
-%                 if(img1_n(i,j) == 1)
-%                     whiteCounter = whiteCounter + 1;
-%                 else
-%                     if(whiteCounter >= 0.5*blackCounter && whiteCounter <= 1.5*blackCounter)
-%                         blackCounter = 0;
-%                         status = 7;
-%                     else
-%                         blackCounter = 0;
-%                         status = 1;
-%                     end
-%                 end
-%             case 7
                 status = 0;
                 hold on
-                horizontal(m,1:4) = [startY, startX, i, j];
-                %Draw line
-                line([horizontal(m,2), horizontal(m,4)], [horizontal(m,1), horizontal(m,3)]);
-                %Calculate k and b of equation y=kx+b
-                k = slope(horizontal(m,1:4));
-                b = intercept(horizontal(m, 1:4), k);
+                horizontal(m,1:4) = [startY, startX, stopY, stopX];
+                 %Draw line
+                 line([horizontal(m,2), horizontal(m,4)], [horizontal(m,1), horizontal(m,3)]);
+%                 %Calculate k and b of equation y=kx+b
+%                 k = slope(horizontal(m,1:4));
+%                 b = intercept(horizontal(m, 1:4), k);
                 horizontal(m,5) = k;
                 horizontal(m,6) = b;
                 
+                midX = startX + (stopX-startX)/2;
+                horizontal(n,5) = midX;
+                
                 m = m+1;
+                plot(midX, startY, '+w');
                 plot(startX, startY, '*b')
                 plot(stopX, stopY, '*y')
-                
         end
     end
 end
